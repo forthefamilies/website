359fa71c54607c1e89388f7af2d4bd0d8db2bed9
Kaleab 470 <kaleab.s.gebremichael@vanderbilt.edu>
2019-03-23 19:11:06 -0500

    Patched up version

---
+++ searchGoogle.js
@@ -0,0 +1,10 @@
+var keywordElement = document.getElementById("keyword");
+var filterElement = document.getElementById("filter");
+var locationCityElement = document.getElementById("city");
+var locationStateRegionElement = document.getElementById("state-region");
+var priceElement = document.getElementById("price");
+
+var googleSearch = function() {
+  var keyword = keywordElement.value;
+  console.log(keyword);
+}
